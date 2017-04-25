6df02c9cb313f5bfbf7ab4c1194a27abf5265f95
Josh Nickell <j.lynx80@gmail.com>
2017-04-25 13:19:50 -0700

    Completed 5.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,48 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+# .swapcase, changes uppercase to lowercase and vice versa, .swapcase! for str or nil
+ "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+# .insert(), insert a character at a given index
+ "zom".insert(2, 'o')
 # => “zoom”
-
-# "enhance".<???>
+# center(width), centers a string, if width is greater than str it will center the str and add padding
+ "enhance".center(15)
 # => "    enhance    "
-
-# "Stop! You’re under arrest!".<???>
+ # .upcase, makes all characters uppercase 
+ "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
-#=> "the usual suspects"
+# .concat, add a string to a string, can also use <<
+ "the usual".concat(" suspects")
+ #=> "the usual suspects"
 
-# " suspects".<???>
+# .prepend, places a string in front of the current string
+ " suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+# .chop, returns str with the last character removed
+ "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+# [1..-1], index the str and take out the character at 0
+ "The mystery of the missing first letter".[1..-1]
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+# split by whitespaces and .gsub with single space
+ "Elementary,    my   dear        Watson!".gsub(/\s+/, " ") 
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+# .ord to return the integer ordinal of a single stirng, .gsub for larger strings
+ "z".ord
+ "zadgooks".gsub(/./){|s|s.ord.to_s + ' '} # =>"122 97 100 103 111 111 107 115"
 # => 122 
-# (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+# (What is the significance of the number 122 in relation to the character z?)
+	#The ordinal number returned is the order of the character in the ASCII table, a = 97, z = 122 
+
+# .count(), count the number of letters and return an integer
+ "How many times does the letter 'a' appear in this string?".count 'a'
 # => 4
