0d9e6ce41ef8e555893785d0f25428bb071c69dc
Josh Nickell <j.lynx80@gmail.com>
2017-04-26 15:45:58 -0700

    5.6 release 1 completed

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,4 @@
+=begin
 arr = [42, 89, 23, 1]
 
 def search_arr(arr, num_value)
@@ -13,3 +14,15 @@ def search_arr(arr, num_value)
 end
 
 p search_arr(arr, 12)
+
+
+def fib(length)
+	arr = [0, 1]
+	for i in 0...length-2
+		arr.push(arr[i] + arr[i+1]) 		
+	end	
+	return arr.last
+end
+
+p fib(100)
+=end
