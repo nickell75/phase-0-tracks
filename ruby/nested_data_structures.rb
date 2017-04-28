cdb623f032e40751f26547abcedc4c7c56475f6a
Josh Nickell <j.lynx80@gmail.com>
2017-04-27 18:56:42 -0700

    Created nested data-structures, Groceries

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,32 @@
+groceries = {
+	meats: {
+		steak: ['Tri-Tip', 'Skirt']
+		chicken: ['Thighs', 'Breast'], 
+		hamburger: ['Patties'],
+		}, 
+	fruits: {
+		apples: []
+		}, 
+	vegies: [
+		'Carrots',
+		'Celery',
+		'Onions'
+		], 
+	drinks: {
+		beer: {
+			corona: 7,
+			heineken: 12,
+			dos_equis: 2
+			},		
+		soad: [
+			'Coca-Cola',
+			'Sprit',
+			'Root Beer'
+			]
+		}, 
+	canned: {
+		soup: 8,
+		peas: 10,
+		peachs: 12
+	}
+}
