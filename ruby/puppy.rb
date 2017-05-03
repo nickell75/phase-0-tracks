fcd2949de3ff33877694f55fd0384b07e6dd9cc9
Josh Nickell <j.lynx80@gmail.com>
2017-05-02 20:30:03 -0700

    Puppy Class, 6.1 release 1

---
+++ ruby/puppy.rb
@@ -0,0 +1,35 @@
+=begin
+	SPECIES ------------------------
+	Dog
+
+	CHARACTERISTICS ----------------
+	Fur length: varies
+	Friendly: yes
+	Leg count: 4
+	Name: varies
+	Color: varies
+
+	BEHAVIOR -----------------------
+	Bark
+	Run/Chase
+	Sleep
+	Pee
+=end
+
+# Puppy Class
+
+class Puppy
+
+end
+
+duchess = Puppy.new 
+
+fido = Puppy.new 
+
+spot = Puppy.new
+
+#spot.class
+#duchess == fido
+#fido.instance_of?(Array)
+#fido.instance_of?(Puppy)
+spot.play_dead
