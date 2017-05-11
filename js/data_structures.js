704de861c2fae8ce88e58eeb95d2590e0fe15c5f
Josh Nickell <j.lynx80@gmail.com>
2017-05-10 17:37:39 -0700

    Completed 7.2 release 3

--- js/data_structures.js
+++ js/data_structures.js
@@ -8,7 +8,27 @@ var horses = {};
 	for (var i = 0; i < colors.length; i++){
 		horses[names[i]] = (colors[i]);
 	}
-
-
-
 console.log(horses);
+
+function car(year, make, model, color){
+	this.year = year;
+	this.make = make;
+	this.model = model;
+	this.color = color;
+
+	//Additional Function
+	this.paint = function(color) {
+		this.color = color;
+		console.log("This " + this.year + " " + this.make + " " + this.model + " painted to be " + this.color);
+	}
+}
+var newCar1 = new car (2014, "Chevy", "Camero", "white");
+var newCar2 = new car (2014, "Chevy", "Tahoe", "blue");
+var newCar3 = new car (2004, "Toyota", "Camry", "black");
+console.log(newCar1);
+console.log(newCar2);
+console.log(newCar3);
+newCar1.paint("Red");
+newCar2.paint("Orange");
+newCar3.paint("Pink");
+
