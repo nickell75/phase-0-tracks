87ecf165348c01d1a292d59a5ccd25475054100d
Josh Nickell <j.lynx80@gmail.com>
2017-05-11 14:24:22 -0700

    Completed first half of release 2

--- js/algos.js
+++ js/algos.js
@@ -14,7 +14,56 @@ function longestString(words){
 	console.log("The longest word in this phrase is: " + temp);
 }
 
+//________PSEUDOCODE________Release 1____
+// Take in two data structures
+//		compare each structure for shared values
+
+function compareObjects(objectOne, objectTwo){
+	console.log("Compared Objects true/false: ")
+  for (var key in objectOne){
+    for (var nextKey in objectTwo){
+      if (key === nextKey){
+        if (objectOne[key] === objectTwo[nextKey]){
+          return true;
+        } 
+      }
+    }
+  }
+  return false;
+}
+
+//________PSEUDOCODE________Release 2____
+// Take in a number for length
+//		Using length, build a data structure of random characters 
+// 		Output the data structure of length size random words
+//			Parameters: Random word length between 1 and 10 letters
+
+function randomWords(number) {
+  var word =[];  
+  for(var j=0; j<number; j++){
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+    var newString = "";
+    for (var i =0; i < Math.random()*10; i++) {
+      newString += (alphabet[Math.round(Math.random() * 10)]);
+    }
+    word.push(newString);
+  }
+  console.log(word);
+}
+
 //___________DRIVER CODE_____________
 // Release 0
-var anotherString = new longestString("This is how we Cowboy BeBop!");
-anotherString;
+//var anotherString = new longestString;
+longestString("This is how we Cowboy BeBop!");
+
+//___________DRIVER CODE_____________
+// Release 1
+console.log(compareObjects({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
+
+console.log(compareObjects({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}));
+
+console.log(compareObjects({animal: "Cat", legs: 4}, {animal: "Dog", legs: 3}));
+
+//___________DRIVER CODE_____________
+// Release 2
+randomWords(3);
