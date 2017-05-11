28fc3dd65c680e81cfdd586b66097ab930db61b9
Josh Nickell <j.lynx80@gmail.com>
2017-05-11 12:40:25 -0700

    Better version of longest word, allows for use of phrase

--- js/algos.js
+++ js/algos.js
@@ -1,18 +1,20 @@
-//________PSEUDOCODE____________
+//________PSEUDOCODE________Release 0____
 // Input words or a phrase and store in a data structure
 //	Search through the data structure for the longest word or phrase
 
 function longestString(words){
 	console.log(words);
-  var temp = "";
-	for (var x = 0; x < words.length; x++){
-	  if (words[x] > temp){
-	  	temp = words[x];
-	  }
-	}
-	console.log(temp);
+	wordSplit = words.split(' ');
+	var temp = "";
+		for (var x = 0; x < wordSplit.length; x++){
+	  		if (wordSplit[x].length > temp.length){
+	  		temp = wordSplit[x];
+	  		}
+		}
+	console.log("The longest word in this phrase is: " + temp);
 }
-//___________DRIVER CODE_____________
 
-var anotherString = new longestString(["Oranges" , "Box", "Apple"]);
+//___________DRIVER CODE_____________
+// Release 0
+var anotherString = new longestString("This is how we Cowboy BeBop!");
 anotherString;
