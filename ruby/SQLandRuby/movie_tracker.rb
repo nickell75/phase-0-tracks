2a5db4e1b09ba514b3e980c710efbdc4c44c5f6d
Josh Nickell <j.lynx80@gmail.com>
2017-05-22 00:24:53 -0700

    Works, some errors, needs work

--- ruby/SQLandRuby/movie_tracker.rb
+++ ruby/SQLandRuby/movie_tracker.rb
@@ -30,98 +30,79 @@ SQL
 ##_____________________Business Logic____________________
 
 
-db.execute(create_table_cmd)
-def add_movie(db, title, rating, comments)
-	db.execute("INSERT INTO movies(title, rating, comments) VALUES(?, ?, ?)", [title, rating, comments])	
-end
-
-def update_movie
-#UPDATE users
-#SET  (field1, field2, field3)
-#   = ('value1', 'value2', 'value3') 
-#WHERE some_condition ;
-end
-
-def top_five(db)
- #db.execute("SELECT * FROM movies WHERE rating = 10")
- puts "\t -- Top Five -- "
- puts "  --Title--Rating--Comments--"
-	movie = db.execute("SELECT * FROM movies WHERE rating = 10")
-	movie.each do |mov|
-		puts "  -  #{mov['title']}  -  #{mov['rating']}  -  #{mov['comments']}"
+	db.execute(create_table_cmd)
+#____Auto Populate table
+	db.execute("INSERT INTO movies(title, rating, comments) VALUES('Terminator', 9, 'Oldy, but a goody')")
+#____Add movies to the table	
+	def add_movie(db, title, rating, comments)
+		db.execute("INSERT INTO movies(title, rating, comments) VALUES(?, ?, ?)", [title, rating, comments])	
 	end
-end
-
-def bottom_five(db)
-	puts "\t -- Worst Five -- "
- 	puts "  --Title--Rating--Comments--"
-	movie = db.execute("SELECT * FROM movies WHERE rating = 1")
-	movie.each do |mov|
-		puts "  -  #{mov['title']}  -  #{mov['rating']}  -  #{mov['comments']}"
+#____Output the top five based on ratings
+	def top_five(db)
+	 puts "\t -- Top Five -- "
+	 puts "  --Title--Rating--Comments--"
+		movie = db.execute("SELECT * FROM movies WHERE rating = 10")
+		movie.each do |mov|
+			puts "  -  #{mov['title']}  -  #{mov['rating']}  -  #{mov['comments']}"
+		end
 	end
-end
-
-def random_movie(db)
-	puts "Random Movie Generator"
-	random = db.execute("SELECT title, rating, comments FROM movies ORDER BY RANDOM() LIMIT 1")
-	random.each do |mov|
-		puts "#{mov['title']} -  #{mov['rating']}  -  #{mov['comments']}"
+#____Output the bottom five based on ratings
+	def bottom_five(db)
+		puts "\t -- Worst Five -- "
+	 	puts "  --Title--Rating--Comments--"
+		movie = db.execute("SELECT * FROM movies WHERE rating = 1")
+		movie.each do |mov|
+			puts "  -  #{mov['title']}  -  #{mov['rating']}  -  #{mov['comments']}"
+		end
 	end
-end
-
-def sort_list(db)
-	#db.execute("SELECT * FROM movies ORDER BY rating ASC")
-end
-
-def print_table(db, title, rating, comments)
-	puts "  --Title--Rating--Comments--"
-	db.execute("SELECT printf('%s-%.2d-%.4d', title, rating, comments) FROM movies ORDER By rating ASC")
-
-	#movie = db.execute("SELECT * FROM movies ORDER BY rating ASC")
-	#movie.each do |mov|
-	#	puts "  -  #{mov['title']}  \t-  #{mov['rating']}  -  #{mov['comments']}"
-	#end
-end
+#____Output a random movie
+	def random_movie(db)
+		puts "\tRandom Movie Generator"
+		puts "  --Title--Rating--Comments--"
+		random = db.execute("SELECT title, rating, comments FROM movies ORDER BY RANDOM() LIMIT 1")
+		random.each do |mov|
+			puts "  -  #{mov['title']} -  #{mov['rating']}  -  #{mov['comments']}"
+		end
+	end
+#____Output the entire movies database
+	def print_table(db, title, rating, comments)
+		puts "  - - Title - - Rating - - Comments--"
+		movie = db.execute("SELECT * FROM movies ORDER BY rating ASC")
+		movie.each do |mov|
+			puts "  -  #{mov['title']} \t-  #{mov['rating']}  -  #{mov['comments']}"
+		end
+	end
 
 ###____________________Driver code_________________________
-
-		puts "What movie would you like to add?"
-			title = gets.chomp
-		puts "Give your movie a rating(1 - 10)"
-			rating = gets.chomp.to_i
-		puts "Would you like to add a review?(Enter N/A if no)"
-			comments = gets.chomp
-add_movie(db, title, rating, comments)
-sort_list(db)# Not Working 
-#top_five(db) - Works fine!!
-#bottom_five(db) - works fine!
-print_table(db, title, rating, comments)
-random_movie(db)
-
-=begin
-print "What would you like to do: "
+input = 0
+while input != 6
+	puts "What would you like to do: "
 	puts "1. Add a movie?"
-	puts "2. Update a Comment or Rating?"
-	puts "3. Checkout Top Five?"
-	puts "4. Checkout Worst Five?"
-	puts "5. Find a movie to watch?"
-	puts ". Or, Just checkout you list of movies?"
+	puts "2. Checkout Top Five?"
+	puts "3. Checkout Worst Five?"
+	puts "4. Find a movie to watch?"
+	puts "5. Or, Just checkout you list of movies?"
+	puts "6. EXIT"
 	puts "Please Enter a number from above - "
 		input = gets.chomp.to_i
-		if input = 1
-			add_movie
-		elsif input = 2
-			update_movie
-		elsif input = 3
-		
-		elsif input = 4
-		
-		elsif input = 5
-		
-		elsif input = 6
-		
-		else
-			puts"Have A Nice Day!"
-		end
-	add_movie
-=end
+	
+			if input == 1
+				puts "What movie would you like to add?"
+					title = gets.chomp
+				puts "Give your movie a rating(1 - 10)"
+					rating = gets.chomp.to_i
+				puts "Would you like to add a review?(Enter N/A if no)"
+					comments = gets.chomp
+					add_movie(db, title, rating, comments)
+			elsif input == 2
+				top_five(db)
+			elsif input == 3
+				bottom_five(db)
+			elsif input == 4
+				random_movie(db)
+			elsif input == 5
+				print_table(db, title, rating, comments)
+			else
+				puts"Have A Nice Day!"
+			end
+end
