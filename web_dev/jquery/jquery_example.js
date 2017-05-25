d17843a026d18c274a09f6d3dd28c514e61f9ee2
Josh Nickell <j.lynx80@gmail.com>
2017-05-25 13:43:27 -0700

    Basic elements release 3 Completed, still working out bugs

---
+++ web_dev/jquery/jquery_example.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	$('#button1').click(function(){	
+    	animateDiv();
+  		animateDivTwo();
+  	}); 
+  	$('#button2').click(function(){
+  		$('.b').fadeIn(300);
+  		$('div.b').attr('class', 'a');  
+  		animateDiv();
+  		animateDivTwo();	
+  	});
+});
+function makeNewPosition(){    
+    var nh = Math.floor(Math.random() * 500);
+    var nw = Math.floor(Math.random() * 1000);    
+    return [nh,nw];        
+}
+function animateDiv(element){
+    var newq = makeNewPosition();
+    var oldq = $('.a').offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+    
+    $(element).animate({ top: newq[0], left: newq[1] }, speed, function(){
+      animateDiv(element);        
+    });    
+};
+function animateDivTwo(){
+    var newq = makeNewPosition();
+    var oldq = $('.a').offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+    
+    $('.a').animate({ top: newq[0], left: newq[1] }, speed, function(){
+      animateDiv(this);        
+    });    
+};
+function calcSpeed(prev, next) {    
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+    
+    var greatest = x > y ? x : y;    
+    var speedModifier = .1;
+    var speed = Math.ceil(greatest/speedModifier);
+
+    return speed;
+}
--- web_dev/jquery/notes.md
+++ web_dev/jquery/notes.md
@@ -9,7 +9,19 @@ Go a little deeper
 	jQuery and Events
 	jQuery and AJAX
 	jQuery and Widgets
+	Watch tutorial videos
 
 Try it!! and Tutorial examples
 	Tutorial point-examples and try it-mess around and manipulate the example provided
 	w3schools - " "
+
+-------Results---------
+
+Researching provided some insite into how jQuery works, but for actual implementation 
+	I found a good video tutorial on youtube that didnt take up to much time. 
+	DevTips - explaned how jQuery works with differnt opening statements, interactions and how
+	to traverse the html DOM.
+
+-----------In Action----------
+
+Even after watching tutorials and read several different resources, there were still a few issues that I didn't understand until actually running the jquery in a js file.  Had problems making the elements behave the way that I wanted and had to fiddle around and look up specific terms in order to make it run the way I wanted. 
